09927359555e674036a43d20505aa9ca03f7491b
luximeng <lucymeng2009@gmail.com>
2022-03-10 22:56:59 +0000

    add greeting message

---
+++ myScript.py
@@ -0,0 +1,7 @@
+#!/user/bin/env python3
+
+#Display greeting
+
+Print = （"Congratulations on making your first step towards buying your dream home. Please follow the steps in this app to enter your offer details"）
+
+
