ae445614a97486c9b745175972ffc052f0a39a91
luximeng <lucymeng2009@gmail.com>
2022-03-11 05:13:28 +0000

    add listedPrice

--- myScript.py
+++ myScript.py
@@ -2,6 +2,21 @@
 
 #Display greeting
 
-Print = （"Congratulations on making your first step towards buying your dream home. Please follow the steps in this app to enter your offer details"）
+print("Congratulations on making your first step towards buying your dream home")
+
+
+#prompt user input for the property asking price (enter the price between $50,000-$15,000,000):
+
+
+
+
+listedPrice =int(input("Please enter the listed price for your target property, between the range of $50,000 - $15,000,000. Listed Price: $"))
+while True:
+    try:
+        print(str("The target property is listed for $ " + listedPrice))
+        if 50000 < listedPrice < 15000000:
+            break
+    except:  
+        print("You entered an unlikely price, please re-enter a price between $50,000 and $15,000,000")
 
 
