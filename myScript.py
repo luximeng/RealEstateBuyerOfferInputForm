73540956897369fefb0cc276bb4115406ca4eda8
luximeng <lucymeng2009@gmail.com>
2022-03-11 06:01:02 +0000

    revise listedPrice

--- myScript.py
+++ myScript.py
@@ -9,14 +9,14 @@ print("Congratulations on making your first step towards buying your dream home"
 
 
 
+#def listedPrice():
 
-listedPrice =int(input("Please enter the listed price for your target property, between the range of $50,000 - $15,000,000. Listed Price: $"))
-while True:
-    try:
-        print(str("The target property is listed for $ " + listedPrice))
-        if 50000 < listedPrice < 15000000:
-            break
-    except:  
-        print("You entered an unlikely price, please re-enter a price between $50,000 and $15,000,000")
+listedPrice = int(input("Please enter the listed price for your target property, between the range of $50,000 - $15,000,000. Listed Price: $"))
+if 50000 < listedPrice < 15000000:
+    print("The target property is listed for $ " + str(listedPrice)) 
+else:
+    print("You entered an unlikely price, please re-enter a price between $50,000 and $15,000,000")
 
+#if __name__ == "__main__":
+ #   listedPrice()
 
